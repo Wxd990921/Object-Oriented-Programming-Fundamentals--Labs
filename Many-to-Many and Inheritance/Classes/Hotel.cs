c9c7edd67055e3636b46e29de32dffef2c3c67e5
Xidi Wang <wangxidi0921@gmail.com>
2023-06-27 20:27:12 -0500

    Lab 04

---
+++ Many-to-Many and Inheritance/Classes/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Many_to_Many_and_Inheritance.Classes
+{
+    public class Hotel
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Room> Rooms { get; set; }
+        public List<Client> Clients { get; set; }
+
+        public Hotel(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Rooms = new List<Room>();
+            Clients = new List<Client>();
+        }
+    }
+}
---
+++ Many-to-Many and Inheritance/Classes/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Many_to_Many_and_Inheritance.Classes
+{
+    public class Room
+    {
+        public string Number { get; set; }
+        public int Capacity { get; set; }
+        public bool Occupied { get; set; }
+        public List<Reservation> Reservations { get; set; }
+
+        public Room(string number, int capacity)
+        {
+            Number = number;
+            Capacity = capacity;
+            Occupied = false;
+            Reservations = new List<Reservation>();
+        }
+    }
+}
